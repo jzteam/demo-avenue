0f8e042523b989b60cbaef0c30d953f9a189c2df
jzteam <zhujizhi1012@163.com>
2018-12-27 21:07:06 +0800

    test

--- src/main/java/com/example/demoavenue/support/schedule/NanGuaYuanTask.java
+++ src/main/java/com/example/demoavenue/support/schedule/NanGuaYuanTask.java
@@ -1,11 +1,23 @@
 package com.example.demoavenue.support.schedule;
 
+import com.example.demoavenue.utils.HttpUtil;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.util.List;
 
 public class NanGuaYuanTask extends AbstractTask {
 
     public static void main(String[] args) {
-        
+        final String page = HttpUtil.getPage("http://www.kan2008.com/thread-305248-1-1.html");
+
+        try(final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/Users/oker/Documents/devote/scratch/nanguayuan/waste/test.html")))){
+            bw.write(page);
+        }catch(Exception e){
+
+        }
+        System.out.println("写入完成");
     }
 
     @Override
