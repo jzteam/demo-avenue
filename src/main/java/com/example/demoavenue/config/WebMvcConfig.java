f3d7f9c10fbb9f4ffb1a7174650fe3acc122c028
jzteam <zhujizhi1012@163.com>
2019-01-18 18:30:49 +0800

    test

--- src/main/java/com/example/demoavenue/config/WebMvcConfig.java
+++ src/main/java/com/example/demoavenue/config/WebMvcConfig.java
@@ -1,8 +1,42 @@
 package com.example.demoavenue.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebMvcConfig {
+    
+    @Bean
+    public TestPerson person(){
+        return new TestPerson("person");
+    }
+
+    @Bean
+    public TestSon son(){
+        return new TestSon("son");
+    }
+    
+    
+    public static class TestPerson {
+        private String name;
+
+        public TestPerson(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    
+    public static class TestSon extends TestPerson{
+        public TestSon(String name) {
+            super(name);
+        }
+    }
 
 }
--- src/test/java/com/example/demoavenue/DemoAvenueApplicationTests.java
+++ src/test/java/com/example/demoavenue/DemoAvenueApplicationTests.java
@@ -1,16 +1,37 @@
 package com.example.demoavenue;
 
+import com.alibaba.fastjson.JSON;
+import com.example.demoavenue.config.WebMvcConfig;
+import com.example.demoavenue.utils.SpringUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoAvenueApplicationTests {
-
+	
 	@Test
 	public void contextLoads() {
+		// 遍历beanDefinitionNames，根据名字找对象，再跟Type匹配，做缓存
+		final Object person = SpringUtil.getBean("person", WebMvcConfig.TestPerson.class);
+		final Object person1 = SpringUtil.getBean("son", WebMvcConfig.TestSon.class);
+//		final Object person2 = SpringUtil.getBean(WebMvcConfig.TestPerson.class);
+		final Object person3 = SpringUtil.getBean(WebMvcConfig.TestSon.class);
+		final Object person4 = SpringUtil.getBean("son", WebMvcConfig.TestPerson.class);
+		final Map<String, WebMvcConfig.TestPerson> beansOfType = SpringUtil.getBeansOfType(WebMvcConfig.TestPerson.class);
+
+		System.out.println("person:"+person.getClass().getName());
+		System.out.println("person1:"+person1.getClass().getName());
+//		System.out.println("person2:"+person2.getClass().getName());
+		System.out.println("person3:"+person3.getClass().getName());
+		System.out.println("person4:"+person4.getClass().getName());
+
+		System.out.println(JSON.toJSONString(beansOfType));
+		
 	}
 
 }
